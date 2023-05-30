5d5a1238dfde299982c8adc60f893883ad865a63
Lina Gallardo <lgallard3@cuc.edu.co>
2023-05-30 14:54:17 -0500

    Cambios firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyBWM1Dcb_gWbHG3GRcyYLwKz-vXchBz5fA",
-  authDomain: "rubrica-web2-aa7fb.firebaseapp.com",
-  projectId: "rubrica-web2-aa7fb",
-  storageBucket: "rubrica-web2-aa7fb.appspot.com",
-  messagingSenderId: "965755189720",
-  appId: "1:965755189720:web:9bf272cf525560a0dcdaad"
+  apiKey: "AIzaSyD7BQboQYekAZH0-vvjYTj5UoV4rL5l8iM",
+  authDomain: "rubrica-bp.firebaseapp.com",
+  projectId: "rubrica-bp",
+  storageBucket: "rubrica-bp.appspot.com",
+  messagingSenderId: "467169634501",
+  appId: "1:467169634501:web:455af05eda4e7ef53e68ac"
 };
 
 // Initialize Firebase
